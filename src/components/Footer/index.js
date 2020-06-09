7280bdbbf5309f8e8ca234ef2f510013e2b13152
pikeno221 <gabriellgomesbh@gmail.com>
2020-06-09 20:30:32 -0300

    footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './styles';
+
+
+function Footer() {
+    return (
+        <S.Container>
+           <span>ToDO - Organizing your life</span>
+        </S.Container>
+    )
+}
+
+export default Footer;
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -3,11 +3,13 @@ import * as S from './styles';
 
 // COMPONENTS
 import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 
 function Home() {
   return (
       <S.Container>
   <Header/>
+  <Footer />
   </S.Container>
   )
 }
