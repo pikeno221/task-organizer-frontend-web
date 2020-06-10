e2afcc8c34b7f0d4a48c70d618517bbdb9807cd0
pikeno221 <gabriellgomesbh@gmail.com>
2020-06-09 21:20:56 -0300

    components filter

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
 
 // COMPONENTS
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import FilterCard from '../../components/FilterCard'
 
 function Home() {
+  const [filterActived, setFilterActived] = useState();
+
   return (
-      <S.Container>
-  <Header/>
-  <Footer />
-  </S.Container>
+    <S.Container>
+      <Header />
+
+      <S.FilterArea>
+        <button type="button" onClick={() => setFilterActived("all")}>
+          <FilterCard title="All" actived={filterActived === 'all'} />
+        </button>
+        <button type="button" onClick={() => setFilterActived("today")}>
+          <FilterCard title="Today" actived={filterActived === 'today'} onClick={() => setFilterActived("today")} />
+        </button>
+        <button type="button" onClick={() => setFilterActived("week")}>
+          <FilterCard title="Week" actived={filterActived === 'week'} onClick={() => setFilterActived("week")} />
+        </button>
+        <button type="button" onClick={() => setFilterActived("month")}>
+          <FilterCard title="Month" actived={filterActived === 'month'} onClick={() => setFilterActived("month")} />
+        </button>
+        <button type="button" onClick={() => setFilterActived("year")}>
+          <FilterCard title="Year" actived={filterActived === 'year'} onClick={() => setFilterActived("year")} />
+        </button>
+
+
+
+
+      </S.FilterArea>
+      <Footer />
+    </S.Container>
   )
 }
 
--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-width:100%
+    width: 100%
+`
+export const FilterArea = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    margin-top: 30px;
+    flex-wrap: wrap;
+
+    button {
+        background: none;
+        border: none;
+    }
+
 `
